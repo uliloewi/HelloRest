d6d9c254e0799309dc658bfdd35091b76b9ea1e3
uliloewi <zequanyu@yahoo.de>
2020-02-21 12:33:32 +0100

    first unit test

---
+++ TestRest/UnitTest.cs
@@ -0,0 +1,27 @@
+using NUnit.Framework;
+using HelloRest.Controllers;
+using Microsoft.Extensions.Logging;
+using System.Collections.Generic;
+using System.Linq;
+using HelloRest;
+
+namespace TestRest
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void TestWeatherForecastGet()
+        {
+            //ILogger<WeatherForecastController> logger = new Logger<WeatherForecastController>(null);
+            var controller = new WeatherForecastController(null);
+            var result =  controller.Get();
+            List<WeatherForecast> s = result.ToList();
+            Assert.IsTrue( s.Count == 5);
+        }
+    }
+}
